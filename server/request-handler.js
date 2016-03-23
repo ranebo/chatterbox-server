b553da2685d1a8ede754f1188a97ddb3412d7355
ranebo <ranegri3@gmail.com>
2016-03-22 17:33:23 -0700

    Comment out code

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,3 +1,4 @@
+// these are for permissions, allowing CORs to go through from sites served on file:// to server on 127.0.0.1:3000
 var headers = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
@@ -6,22 +7,29 @@ var headers = {
   'Content-Type': 'application/json'
 };
 
+// include node module dependency
 var urlParse = require('../node_modules/url');
+
+// storage variables for server data
 var results = [];
 var objectIdCounter = 1;
 
+// reusable function for various response types
 var sendResponse = function(response, data, statusCode) {
   statusCode = statusCode || 200;
   response.writeHead(statusCode, headers);
   response.end(JSON.stringify(data));
 };
 
+// callback function invoked by basic-server.js
 var requestHandler = function(request, response) {
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
   headers['Content-Type'] = 'application/json';
 
+  // use url module to parse the relative path of the request
   var currentPath = urlParse.parse(request.url).pathname;
 
+  // set up whitelist of URLs to accept requests at
   var UrlsAllowed = ['/classes/messages/', '/send', '/classes/room', '/messages', '/log', '/classes/messages'];
 
   if (UrlsAllowed.indexOf(currentPath) < 0) {
@@ -32,17 +40,18 @@ var requestHandler = function(request, response) {
     sendResponse(response, {results: results});
   } else if (request.method === 'POST') {
     var dataString = '';    
-    request.on('data', function(chunk) {
-      dataString += chunk;
+    request.on('data', function(chunk) { // data comes in one chunk at a time
+      dataString += chunk; // build up the data string until it is complete
     });
-    request.on('end', function(data) {
-      var message = JSON.parse(dataString);
-      message.objectId = ++objectIdCounter;
-      results.push(message);
+    request.on('end', function(data) { // once data string is complete,
+      var message = JSON.parse(dataString); // parse it into a message object
+      message.objectId = ++objectIdCounter; // give each message a unique ID
+      results.push(message); // add message to storage
       sendResponse(response, {results: results}, 201);
     });
   }
 
 };
 
+// make callback function available to basic-server.js
 exports.requestHandler = requestHandler;
